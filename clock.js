2880e5b93d5fa7b4e5f109a1996139f6cb08bccd
royce-u <royceubando@gmail.com>
2020-03-23 19:10:46 -0700

    finished req

---
+++ clock.js
@@ -0,0 +1,27 @@
+var secondDegrees = 0
+var minuteDegrees = 0
+var hourDegrees = 0
+
+function secondRotation() {
+    secondDegrees += 6
+    secondsHand.style.transform = "rotate(" + secondDegrees + "deg)"
+}
+
+function minuteRotation() {
+    minuteDegrees += 6
+    minutesHand.style.transform = "rotate(" + minuteDegrees + "deg)"
+}
+
+function hourRotation() {
+    hourDegrees += 30
+    hourHand.style.transform = "rotate(" + minuteDegrees + "deg)"
+}
+
+var secondsHand = document.getElementById('second')``
+var minutesHand = document.getElementById('minute')
+var hourHand = document.getElementById('hour')
+setInterval(secondRotation, 1000);
+setInterval(minuteRotation, 3600000);
+
+// console.log(secondDegrees(60))
+
